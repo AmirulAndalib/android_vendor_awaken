39b26a8e9ad8b22f0746fe44efc840596c307e5c
Ido Ben-Hur <idoybh2@gmail.com>
2023-03-02 21:57:53 +0200

    vendor: Conditionally enable dev keys signing
    Change-Id: Ic3a62a1c19de618dacae99b797633f487fc3e4f0
    
    -----
    
    signing: support inline signing for U [2/2]
    
    For those who have used .android-certs previously, cp .android-certs/* vendor/awaken/signing/keys/
    
    Change-Id: I7780cb256348065aa279459f598adaa265a2eb67
    Co-authored-by: John Galt <johngaltfirstrun@gmail.com>

--- config/branding.mk
+++ config/branding.mk
@@ -31,9 +31,15 @@ PRODUCT_COPY_FILES += \
     vendor/awaken/prebuilt/common/etc/init/init.awaken-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.awaken-updater.rc
 endif
 
-# Sign builds if building an official build
-ifeq ($(filter-out official,$(AWAKEN_BUILD_TYPE)),)
-    PRODUCT_DEFAULT_DEV_CERTIFICATE := $(KEYS_LOCATION)
+# Signing
+ifneq (eng,$(TARGET_BUILD_VARIANT))
+ifneq (,$(wildcard vendor/awaken/signing/keys/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/awaken/signing/keys/releasekey
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+endif
+ifneq (,$(wildcard vendor/awaken/signing/keys/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := vendor/awaken/signing/keys/otakey.x509.pem
+endif
 endif
 
 # Set all versions
